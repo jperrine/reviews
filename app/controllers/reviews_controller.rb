28bd960d5c8bff87717246806c0989461af8a96e
Jimmy Perrine <jimmy.perrine@gmail.com>
2012-10-04 17:03:10 -0400

    merged two index actions into one

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,8 +2,21 @@ class ReviewsController < ApplicationController
   before_filter :authenticate_user!, only: [:index, :hide, :approve]
 
   def index
-    @reviewable_object = current_user.reviewable_objects.find(params[:reviewable_object_id])
-    @reviews = @reviewable_object.reviews
+    respond_to do |format|
+      format.html do
+        @reviewable_object = current_user.reviewable_objects.find(params[:reviewable_object_id])
+        @reviews = @reviewable_object.reviews
+      end
+
+      format.json do
+        reviewable_object = ReviewableObject.where(widget_key: params[:key]).first unless params[:key].blank?
+        if params[:key].blank? || reviewable_object.blank?
+          render json: { errors: "Invalid reviewable_object_widget_key" }, status: 406
+        else
+          render json: reviewable_object.reviews, status: 200
+        end
+      end
+    end
   end
 
   def hide
@@ -30,18 +43,6 @@ class ReviewsController < ApplicationController
         format.json { render json: @review, status: 200 }
       else
         format.json { render json: {errors: "review could not be saved"}, status: 406 }
-      end
-    end
-  end
-
-  def index
-    reviewable_object = ReviewableObject.where(widget_key: params[:key]).first unless params[:key].blank?
-
-    respond_to do |format|
-      if params[:key].blank? || reviewable_object.blank?
-        format.json { render json: {errors: "Invalid reviewable_object_widget_key"}, status: 406 }
-      else
-        format.json { render json: reviewable_object.reviews, status: 200 }
       end
     end
   end
